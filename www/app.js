cf70574faedd0239e135d9dbc88e86876e849864
Louise Beh <louise.beh@gmail.com>
2015-03-27 10:36:03 +0000

    refactored app.js

--- www/app.js
+++ www/app.js
@@ -1,120 +1,116 @@
 var netherTest = angular.module('NetherTest', ['ngResource', 'ngRoute', 'LocalStorageModule'])
 .config(['localStorageServiceProvider', function(localStorageServiceProvider){
-  localStorageServiceProvider.setPrefix('ls');
+ localStorageServiceProvider.setPrefix('ls');
 }])
 .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+   $routeProvider
+     .when('/', {
+       templateUrl: 'views/main.html',
+       controller: 'MainCtrl'
+     })
+     .when('/about', {
+       templateUrl: 'views/about.html',
+       controller: 'AboutCtrl'
+     })
+     .otherwise({
+       redirectTo: '/'
+     });
+ });
 
 'use strict';
 
 netherTest.controller('NetherTestController', function($scope, $resource, $interval, localStorageService) {
 
-  var averageSpeed = 30; var refreshInterval = 1000; var proximity = 0.5;
-
-  var savedLocations = localStorageService.get('savedLocations');
-  $scope.savedLocations = savedLocations || [];
-
-  $scope.$watch('savedLocations', function() { localStorageService.set('savedLocations', $scope.savedLocations); }, true);
-
-  $scope.locCoords = {lat: 0, lon: 0, area: 'unknown'};
-  $scope.destCoords = {lat: 0, lon: 0, area: 'unknown'};
-  $scope.sleeping = false;
-  $scope.arrived = false;
-
-  var input = document.getElementById('search');
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  var geocoder = new google.maps.Geocoder();
+ var savedLocations = localStorageService.get('savedLocations');
+ $scope.savedLocations = savedLocations || [];
+ $scope.$watch('savedLocations', function() { localStorageService.set('savedLocations', $scope.savedLocations); }, true);
 
 
-  function updateLocation() {
-    navigator.geolocation.getCurrentPosition(showPosition);
-    getDistanceBetween($scope.locCoords.lat, $scope.locCoords.lon, $scope.destCoords.lat, $scope.destCoords.lon);
-    getEstimatedSleep();
-  };
+ var averageSpeed = 15; var refreshInterval = 5000; var proximity = 1;
+ var defaultLocation = { location: 'loading', lon: 0, lat: 0, area: 'loading'};
 
-  function showPosition(position) {
-    $scope.locCoords.lat = position.coords.latitude;
-    $scope.locCoords.lon = position.coords.longitude;
-    getCurrentLocation();
-    checkArrived();
-  };
+ var input = document.getElementById('search');
+ var autocomplete = new google.maps.places.Autocomplete(input);
+ var geocoder = new google.maps.Geocoder();
 
-  $scope.sleep = function(location) {
-    $scope.sleeping = true;
-    $scope.promise = $interval(updateLocation, refreshInterval);
-    if (location) {
-      $scope.destCoords = {lat: location.lat,
-                           lon: location.lon,
-                           area: location.area};
-    } else {
-      var new_input = $('#search').val();
-      geocoder.geocode({'address': new_input}, function(results, status) {
+ initVariables();
 
-        if (status == google.maps.GeocoderStatus.OK) {
-          $scope.destCoords = {lat: results[0].geometry.location.lat(),
-                               lon: results[0].geometry.location.lng(),
-                               area: results[0].formatted_address}
-          $scope.savedLocations.push({location: new_input,
-                                     lat: results[0].geometry.location.lat(),
-                                     lon: results[0].geometry.location.lng(),
-                                     area: results[0].formatted_address});
-        };
-      })
-    };
-    $scope.destination = '';
-  };
+ function updateLocation() {
+   navigator.geolocation.getCurrentPosition(getLocCoords);
+   getCurrentLocation();
+   getStraightLineDistance();
+   getEstimatedSleep();
+   checkArrived();
+ };
 
-  $scope.cancelSleep = function() {
-    $scope.sleeping = false;
-    $interval.cancel($scope.promise);
-    $scope.locCoords = { lat: 0, lon: 0, area: 'unknown' };
-    $scope.destCoords = { lat: 0, lon: 0, area: 'unknown' };
-    $scope.arrived = false;
-  };
+ function getLocCoords(position) {
+   $scope.locCoords.lat = position.coords.latitude;
+   $scope.locCoords.lon = position.coords.longitude;
+ };
 
-function getDistanceBetween(lat1,lon1,lat2,lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2-lat1);  // deg2rad below
-    var dLon = deg2rad(lon2-lon1);
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c; // Distance in km
-    $scope.distanceBetween = Math.round(d * 10) / 10;
+ function getCurrentLocation() {
+   var areaSearch = $resource('http://pelias.mapzen.com/reverse?', {lat: $scope.locCoords.lat, lon: $scope.locCoords.lon});
+   try { areaSearch.get(function (data) { $scope.locCoords.area = data.features[0].properties.text; }); }
+   catch(e) {};
+ };
 
-    if (d < 0.5) {
-      navigator.notification.vibrate(2500);
-      navigator.notification.beep(3);
-        };
-  };
+ $scope.sleep = function(location) {
+   $scope.sleeping = true;
+   if (location) { $scope.destCoords = location; }
+   else { getNewLocationWith($('#search').val()); };
+   $scope.promise = $interval(updateLocation, refreshInterval);
+ };
 
-  function getEstimatedSleep() {
-    $scope.estimatedSleep = Math.round(60 * $scope.distanceBetween / averageSpeed)*2;
-  };
+ function getNewLocationWith(new_input) {
+   geocoder.geocode({'address': new_input}, function(results, status) {
+     $scope.destCoords = {location: new_input, lat: results[0].geometry.location.lat(), lon: results[0].geometry.location.lng(), area: results[0].formatted_address};
+     saveLocation($scope.destCoords);
+   })
+ };
 
-  function getCurrentLocation() {
-    var areaSearch = $resource('http://pelias.mapzen.com/reverse?', {lat: $scope.locCoords.lat, lon: $scope.locCoords.lon});
-    areaSearch.get(function (data) { $scope.locCoords.area = data.features[0].properties.text; });
-  };
+ function saveLocation(newLocation) {
+   $scope.savedLocations.push(newLocation);
+   if ($scope.savedLocations.length > 3) $scope.savedLocations.shift();
+ };
 
-  function deg2rad(deg) {
-    return deg * (Math.PI/180)
-  };
+ $scope.cancelSleep = function() {
+   $interval.cancel($scope.promise);
+   initVariables();
+ };
 
-  function checkArrived() {
-    if ($scope.distanceBetween < proximity) $scope.arrived = true;
-  };
+ function getStraightLineDistance() {
+   var lat1 = $scope.locCoords.lat; var lon1 = $scope.locCoords.lon; var lat2 = $scope.destCoords.lat; var lon2 = $scope.destCoords.lon;
+   var R = 6371; var dLat = deg2rad(lat2-lat1); var dLon = deg2rad(lon2-lon1);
+   var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
+   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+   var d = R * c; // Distance in km
+   $scope.distanceBetween = Math.round(d * 10) / 10;
+ };
+
+ function deg2rad(deg) {
+   return deg * (Math.PI/180);
+ };
+
+ function getEstimatedSleep() {
+   $scope.estimatedSleep = Math.round(60 * $scope.distanceBetween / averageSpeed);
+ };
+
+ function checkArrived() {
+   if ($scope.distanceBetween < proximity) { $scope.arrived = true; alertPhone(); };
+ };
+
+ function alertPhone() {
+   navigator.notification.vibrate(2500);
+   navigator.notification.beep(3);
+ };
+
+ function initVariables() {
+   $scope.locCoords   = defaultLocation;
+   $scope.destCoords  = defaultLocation;
+   $scope.sleeping    = false;
+   $scope.arrived     = false;
+   $scope.destination = '';
+ };
 
 
 var theToggle = document.getElementById('toggle');
